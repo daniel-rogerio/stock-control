651ff73e641cfe0631a0bc93b5cae16dac0a9a65
daniel-rogerio <danielr.xp@gmail.com>
2024-11-25 15:05:46 -0300

    Feat: incluído serviço de cookies.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CookieService } from 'ngx-cookie-service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,7 +32,7 @@ import { ToastModule } from 'primeng/toast'
     ButtonModule,
     ToastModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
